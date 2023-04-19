21529f105f38762299e4316969b3a9633abf1fd4
xISSAx <86708276+xISSAx@users.noreply.github.com>
2023-04-19 07:34:40 -0400

    Delete Alpha-Co-Vision directory

--- Alpha-Co-Vision/config.py
+++
@@ -1,3 +0,0 @@
-# Store Your API keys.
-cohere_api_key = "YOUR_COHERE_API_KEY" #USE COHERE_AI PRODUCTION API FIND FOUT MORE --> : https://dashboard.cohere.ai/api-keys
-openai_api_key = "YOUR_OPENAI_API_KEY" # GET YOUR OPENAI API KEY HERE: #https://platform.openai.com/account/api-keys
