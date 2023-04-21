06ab3de35cd8addae510545b4c6fd74bf81ce574
Varun Kumethi <86708276+xISSAx@users.noreply.github.com>
2023-04-21 19:12:33 -0400

    [Minor] Update main.py
    summary of changes in main.py:
    
    1. Added `previous_responses` list to store previous responses and avoid repetitions.
    2. Updated the `process_frame()` function to include the `previous_responses` list and pass it to the `generate_response()` function.
    3. Modified the `display_frame()` function to flip the frame horizontally, creating a mirrored display for the video without affecting the input to the model.

--- main.py
+++ main.py
@@ -5,37 +5,53 @@ from image_processing import convert_frame_to_pil_image
 from caption_generation import generate_caption
 from response_generation import generate_response
 
-# initialize variables, e.g., cap, last_process_time, etc.
+# create VideoCapture object for camera feed
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 25)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 25)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # set resolution to 640x280
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 280)
 
+# initialize variable for tracking processing times
 last_process_time = time.time()
 last_generation_time = time.time()
-previous_caption = ""
+previous_caption = ""  # initialize variables for tracking previous captions and responses
 previous_response = ""
 previous_captions = []
-lock = threading.Lock()
+previous_responses = []
+lock = threading.Lock()  # initialize lock for threading synchronization
 
-# Processing each frame in a separate thread
+
+# convert frame to PIL image format and generate caption for a frame
 def process_frame(frame):
-    global last_generation_time, previous_captions
+    global last_generation_time, previous_captions, previous_responses
     pil_image = convert_frame_to_pil_image(frame)
     caption = generate_caption(pil_image)
 
-    # Generate a response every 3 seconds if a new caption has been generated
-    current_time = time.time()
-    if current_time - last_generation_time >= 3: # Increase this variable if you want slower LLM outputs
+    current_time = time.time() # track current time for processing time comparison
+    if current_time - last_generation_time >= 2:  # generate response every 2 seconds
         if caption and caption not in previous_captions:
-            previous_captions.append(caption)
-            response = generate_response(previous_caption + " " + caption, previous_response)
-            print(response)
-        last_generation_time = current_time
+            previous_captions.append(caption) # add caption to previous captions list
+            if len(previous_captions) > 10: # limit previous captions list to 10 items
+                previous_captions.pop(0)
 
-# Displaying processed frame
+            response = generate_response(previous_caption + " " + caption, previous_response, previous_responses)  # generate response for caption and previous response
+
+            while response in previous_responses: # ensure response is unique
+                response = generate_response(previous_caption + " " + caption, previous_response)
+
+            previous_responses.append(response) # add response to previous responses list
+            if len(previous_responses) > 10:
+                previous_responses.pop(0)
+
+            print(response) # print response to console
+        last_generation_time = current_time # update last generation time
+
+
 def display_frame(frame):
+    """
+    Function to display a frame on the screen and overlay the previous captions on top of it.
+    """
     global previous_captions
-    with lock:
+    with lock: # synchronize with lock
         font = cv2.FONT_HERSHEY_SIMPLEX
         font_scale = 0.0
         thickness = 1
@@ -44,8 +60,10 @@ def display_frame(frame):
         previous_captions_str = '\n'.join(previous_captions)
         cv2.putText(frame, previous_captions_str, org, font, font_scale, color, thickness, cv2.LINE_AA)
         cv2.imshow('frame', frame)
+        flipped_frame = cv2.flip(frame, 1) # Flip the frame horizontally
+        cv2.imshow('frame', flipped_frame)
 
-# The main loop of the program
+#The LOOOOOOP
 def main_loop():
     global last_process_time
     while True:
@@ -55,7 +73,7 @@ def main_loop():
             break
 
         current_time = time.time()
-        if current_time - last_process_time >= 2: #Tweak this variable should you want more or less less images processed.
+        if current_time - last_process_time >= 2:
             t = threading.Thread(target=process_frame, args=(frame,))
             t.start()
             last_process_time = current_time
@@ -67,6 +85,7 @@ def main_loop():
 
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main_loop()
