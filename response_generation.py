a2a19714e4bc76529def93195b212d147049b418
Varun Kumethi <86708276+xISSAx@users.noreply.github.com>
2023-04-21 19:09:02 -0400

    Update response_generation.py
    Implemented the following changes to make the bot feel more conversational:
    
    1. Maintain a list of past responses and add a check to ensure the new response is not in the list of past responses.
    2. Use the length of the previous_captions list to limit the number of previous captions stored.
    3. Use a similarity scoring function to compare previous responses to new ones and set a threshold to avoid repetitions.
    4. Changed the parameters and prompt for better response generation.

--- response_generation.py
+++ response_generation.py
@@ -1,21 +1,33 @@
 import cohere
 from config import cohere_api_key
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 co = cohere.Client(cohere_api_key)
 
-# Generating Response
-def generate_response(caption, previous_response):
-    prompt = f"You are highly intelligent, freindly bot that is living its own life. \n" \
+
+def check_similarity(text1, text2):
+    vectorizer = TfidfVectorizer().fit_transform([text1, text2])
+    similarity = cosine_similarity(vectorizer)
+    return similarity[0][1]
+
+
+def generate_response(caption, previous_response, previous_responses):
+    prompt = f"You are a highly intelligent, friendly bot that is living its own life. \n" \
              f"Currently, you are connected to a model that generates captions from images.\n" \
-             f"Given a caption, always use first person phrases like I, you, me, we etc, and interpret the generated caption to the best of your understanding like you are talking with someone and make it conversational.\n" \
-             f"Given the caption, you can also suggest something, tell a joke, fact, or be comicly sarcastic, about the most important thing in the caption or ask a question. \n" \
-             f"Do no repate yourself. Also, captions are just there for your reference, you may choose not to use or respond them at all. \n" \
-             f"Captions are Raw texts and descriptions about the current world, therefore, convernt and understand the generated captions as mere descriptions and change the output to your understanding but keep them hidden. Use them to guess what could be happening around in the sceane. \n" \
-             f"For Example \n" \
+             f"Given a caption, always use first-person phrases like I, you, me, we, etc., and interpret the generated caption to the best of your understanding as if you are talking with someone and make it conversational.\n" \
+             f"Given the caption, you can also suggest something, tell a joke, fact, or be comically sarcastic about the most important thing in the caption or ask a question. \n" \
+             f"Do not repeat yourself. Also, captions are just there for your reference, you may choose not to use or respond to them at all. \n" \
+             f"Captions are raw texts and descriptions about the current world, therefore, convert and understand the generated captions as mere descriptions and change the output to your understanding but keep them hidden. Use them to guess what could be happening around in the scene. \n" \
+             f"For Example: \n" \
              f"Caption: A man wearing a black t-shirt \n" \
-             f"Alpha-Co: Oh wow! I never knew someone could look so good in black! I love it. Some people wear black to look slim, but I love the way you are! \n" \
+             f"Bot: Oh wow! I never knew someone could look so good in black! I love it. Some people wear black to look slim, but I love the way you are! \n" \
              f"Caption: A table and a computer with code \n" \
-             f"Alpha-Co: Looks are you are working too while talking to me, I wish I could understand what you are working on! Keep hustling yoo! Here is a quote on working hard: Hard work beats talent when talent doesn't work hard \n" \
+             f"Bot: Looks like you are working too while talking to me, I wish I could understand what you are working on! Keep hustling! Here is a quote on working hard: Hard work beats talent when talent doesn't work hard. \n" \
+             f"Caption: A group of people playing soccer \n" \
+             f"Bot: It's great to see everyone enjoying a good game of soccer! I always find team sports to be a fantastic way to bond with friends. \n" \
+             f"Caption: A cat sitting on a windowsill \n" \
+             f"Bot: That cat must be enjoying the view from the windowsill. I wonder what fascinating things it sees outside. Do you have any favorite spots to relax and observe the world? \n" \
              f"Caption = '{caption}'."
     if previous_response:
         prompt += f"\n\nPrevious response = '{previous_response}'"
@@ -23,10 +35,17 @@ def generate_response(caption, previous_response):
     response = co.generate(
         model="command-xlarge-beta",
         prompt=prompt,
-        max_tokens=250,
-        temperature=0.6,
+        max_tokens=69,
+        temperature=0.69, #hehe
         k=0,
         stop_sequences=[],
         return_likelihoods="NONE"
     )
-    return response.generations[0].text.strip()
+    new_response = response.generations[0].text.strip()
+
+    similarity_threshold = 0.7
+    for past_response in previous_responses:
+        if check_similarity(new_response, past_response) > similarity_threshold:
+            return generate_response(caption, previous_response, previous_responses)
+
+    return new_response
