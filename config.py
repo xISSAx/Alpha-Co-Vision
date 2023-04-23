de1b8c21e76345a5410bfcdd5f2ea7abdbffd72f
Varun Kumethi <86708276+xISSAx@users.noreply.github.com>
2023-04-22 23:44:11 -0400

    Update Config.py
    - Commented out to eliminate openai key error.
    - updated instructions in comments.

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-# Store Your API keys.
+# Store Your API keys. #Use Production key for best expereince. Not sure but you might receive upto $60-$70 worth of credits initially, checkout Cohere for more info.
 cohere_api_key = "YOUR_COHERE_API_KEY" #USE COHERE_AI PRODUCTION API FIND FOUT MORE --> : https://dashboard.cohere.ai/api-keys
-openai_api_key = "YOUR_OPENAI_API_KEY" # GET YOUR OPENAI API KEY HERE: #https://platform.openai.com/account/api-keys
+
+#Should you wish to use OpenAI GPT-3 API, please uncomment the following, and change response_generation to openai_response_generation in main.py.
+#Warning high $$$ spend on OpenAI due to the use of GPT-3 instruct davinci - 003, please change the model in response_generation if you want less $ spend before proceeding.
+#openai_api_key = "YOUR_OPENAI_API_KEY" # GET YOUR OPENAI API KEY HERE: #https://platform.openai.com/account/api-keys
