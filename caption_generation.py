f2b80ca83b1ef36ab2254757e2581cb73244c924
Varun Kumethi <86708276+xISSAx@users.noreply.github.com>
2023-05-11 12:33:41 -0400

    Update caption_generation.py
    -Minor code and spacing fixes

--- caption_generation.py
+++ caption_generation.py
@@ -3,7 +3,8 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 
 # Load the image captioning model and processor from the pretrained model
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base", torch_dtype=torch.float16).to("mps")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base",torch_dtype=torch.float16).to("mps")
+
 
 # Generate captions for an input image
 def generate_caption(pil_image):
@@ -16,3 +17,4 @@ def generate_caption(pil_image):
         return caption
     except:
         return "Unable to process image."
+    
